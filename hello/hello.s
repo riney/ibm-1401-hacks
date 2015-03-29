c719e4c6bc1732a79ca2e70a464c1aa97933618a
John Riney <john.riney@pokitdok.com>
2015-03-28 21:49:09 -0400

    saved an unnecessary instruction

--- hello/hello.s
+++ hello/hello.s
@@ -6,7 +6,6 @@
                CTL  6611
                DCW  @HELLO WORLD@
      START     CS   299           * CLEAR PRINT AREA
-               CS   300
                MCW  343,211       * MOVE TEXT TO PRINT AREA
                W                  * AND PRINT
      HALT      H                  * THEN STOP
