f34a2eaeb0c95892ba8f10647f64404456b229c9
John Riney <john.riney@pokitdok.com>
2015-03-28 22:00:25 -0400

    labels! moar text!

--- hello/hello.s
+++ hello/hello.s
@@ -4,9 +4,9 @@
      *
                JOB  HELLO WORLD
                CTL  6611
-               DCW  @HELLO WORLD@
+     TEXT      DCW  @HELLO WORLD, I AM THE IBM 1401!@
      START     CS   299           * CLEAR PRINT AREA
-               MCW  343,211       * MOVE TEXT TO PRINT AREA
+               MCW  TEXT,231      * MOVE TEXT TO PRINT AREA
                W                  * AND PRINT
-     HALT      H                  * THEN STOP
+     HALT      H    HALT          * THEN STOP
                END  START         * LAST CARD: SPECIFY ENTRY POINT
